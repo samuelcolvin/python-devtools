b727c090531e4b203409a4a02f0e12bbf40e3db6
Samuel Colvin <s@muelcolvin.com>
2018-12-28 20:00:19 +0000

    fix python version checks

--- tests/test_prettier.py
+++ tests/test_prettier.py
@@ -211,7 +211,7 @@ def test_deep_objects():
 )"""
 
 
-@pytest.mark.skipif(sys.version_info >= (3, 6), reason='like this only for 3.5')
+@pytest.mark.skipif(sys.version_info > (3, 5, 3), reason='like this only for old 3.5')
 def test_call_args_py35():
     m = MagicMock()
     m(1, 2, 3, a=4)
@@ -224,7 +224,7 @@ _Call(
 )"""
 
 
-@pytest.mark.skipif(sys.version_info < (3, 6), reason='different for 3.5')
+@pytest.mark.skipif(sys.version_info <= (3, 5, 3), reason='different for old 3.5')
 def test_call_args_py36():
     m = MagicMock()
     m(1, 2, 3, a=4)

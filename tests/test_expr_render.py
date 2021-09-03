a62129309df491086175b3635f1a43092bf65594
Samuel Colvin <s@muelcolvin.com>
2021-09-03 18:39:35 +0100

    tweak async test

--- tests/test_expr_render.py
+++ tests/test_expr_render.py
@@ -11,7 +11,7 @@ def foobar(a, b, c):
     return a + b + c
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_simple():
     a = [1, 2, 3]
     v = debug.format(len(a))
@@ -23,7 +23,7 @@ def test_simple():
     ) == s
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_subscription():
     a = {1: 2}
     v = debug.format(a[1])
@@ -34,7 +34,7 @@ def test_subscription():
     ) == s
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_exotic_types():
     aa = [1, 2, 3]
     v = debug.format(
@@ -83,7 +83,7 @@ def test_exotic_types():
     ) == s
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_newline():
     v = debug.format(
         foobar(1, 2, 3))
@@ -95,7 +95,7 @@ def test_newline():
     ) == s
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_trailing_bracket():
     v = debug.format(
         foobar(1, 2, 3)
@@ -108,7 +108,7 @@ def test_trailing_bracket():
     ) == s
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_multiline():
     v = debug.format(
         foobar(1,
@@ -123,7 +123,7 @@ def test_multiline():
     ) == s
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_multiline_trailing_bracket():
     v = debug.format(
         foobar(1, 2, 3
@@ -136,7 +136,7 @@ def test_multiline_trailing_bracket():
     ) == s
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 @pytest.mark.skipif(sys.version_info < (3, 6), reason='kwarg order is not guaranteed for 3.5')
 def test_kwargs():
     v = debug.format(
@@ -153,7 +153,7 @@ def test_kwargs():
     ) == s
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 @pytest.mark.skipif(sys.version_info < (3, 6), reason='kwarg order is not guaranteed for 3.5')
 def test_kwargs_multiline():
     v = debug.format(
@@ -171,7 +171,7 @@ def test_kwargs_multiline():
     ) == s
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_multiple_trailing_lines():
     v = debug.format(
         foobar(
@@ -184,7 +184,7 @@ def test_multiple_trailing_lines():
     ) == s
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_very_nested_last_statement():
     def func():
         return debug.format(
@@ -208,7 +208,7 @@ def test_very_nested_last_statement():
     )
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_syntax_warning():
     def func():
         return debug.format(
@@ -261,7 +261,7 @@ def test_no_syntax_warning():
     )
 
 
-@pytest.mark.xfail(sys.platform == 'win32', reason='yet unknown windows problem')
+@pytest.mark.xfail(sys.platform == 'win32', reason='as yet unknown windows problem')
 def test_await():
     async def foo():
         return 1
@@ -269,8 +269,9 @@ def test_await():
     async def bar():
         return debug.format(await foo())
 
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     v = loop.run_until_complete(bar())
+    loop.close()
     s = re.sub(r':\d{2,}', ':<line no>', str(v))
     assert (
         'tests/test_expr_render.py:<line no> bar\n'

e4f4edf4dfd90275e3e4aa4721d025e9b0c0c6bf
Samuel Colvin <s@muelcolvin.com>
2022-07-26 14:22:10 +0100

    switch to hatch/hatchling

--- devtools/__init__.py
+++ devtools/__init__.py
@@ -3,11 +3,6 @@ from .ansi import *
 from .debug import *
 from .prettier import *
 from .timer import *
-
-try:
-    from .version import VERSION
-except ImportError:
-    # version is set by running .github/set_version.py to create devtools/version.py
-    VERSION = '0'
+from .version import VERSION
 
 __version__ = VERSION
